01cd515e0bec643f9109d5b76f4828280cdabf77
namanh2004 <anhnknph36045@fpt.edu.vn>
2023-12-06 07:35:51 +0700

    namanhup

--- app/src/main/java/com/example/duan1/Database/DbHelper.java
+++ app/src/main/java/com/example/duan1/Database/DbHelper.java
@@ -7,7 +7,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DbHelper extends SQLiteOpenHelper {
     public static final String DB_NAME = "MobileManager";
-    public static final int VER_SION = 24;
+    public static final int VER_SION = 28;
 
 
     public DbHelper(Context context) {
@@ -35,12 +35,7 @@ public class DbHelper extends SQLiteOpenHelper {
         db.execSQL(createTableKhachHang);
 
 
-        //Tạo bảng Hãng
-        String createTableHang = "CREATE TABLE Hang(" +
-                "maHang TEXT NOT NULL UNIQUE PRIMARY KEY," +
-                "tenHang TEXT NOT NULL)" ;
 
-        db.execSQL(createTableHang);
 
         //Tạo bảng Sản Phẩm
         String createTableSanPham = "CREATE TABLE SanPham(" +
@@ -53,14 +48,7 @@ public class DbHelper extends SQLiteOpenHelper {
         db.execSQL(createTableSanPham);
 
 
-        String createTableCTSP = "CREATE TABLE CTSP(" +
-                "maCTSP TEXT PRIMARY KEY," +
-                "maSP INTEGER REFERENCES SanPham(maSP)," +
-                "tenCTSP TEXT NOT NULL," +
-                "hangCTSP TEXT NOT NULL," +
-                "giaTien INTEGER NOT NULL,"+
-                "moTaCTSP TEXT NOT NULL)";
-        db.execSQL(createTableCTSP);
+
 
         //giỏ hàng
         String createTableGioHang = "CREATE TABLE GioHang(" +
@@ -103,22 +91,22 @@ public class DbHelper extends SQLiteOpenHelper {
         // Xóa bảng khi update VERSION
         String dropTableLoaiQuanTriVien = "Drop table if exists QuanTriVien";
         String dropTableLoaiKhachHang = "Drop table if exists KhachHang";
-        String dropTableLoaiHang = "Drop table if exists Hang";
+
         String dropTableLoaiSanPham = "Drop table if exists SanPham";
         String dropTableLoaiHoaDon = "Drop table if exists HoaDon";
         String dropTableLoaiChiTietHoaDon = "Drop table if exists ChiTietHoaDon";
 
-        String dropTableCTSanPham = "Drop table if exists CTSP";
+
         String dropTableGioHang = "Drop table if exists GioHang";
 
         if (oldVersion != newVersion) {
             db.execSQL(dropTableLoaiQuanTriVien);
            db.execSQL(dropTableLoaiKhachHang);
-            db.execSQL(dropTableLoaiHang);
+
             db.execSQL(dropTableLoaiSanPham);
             db.execSQL(dropTableLoaiHoaDon);
             db.execSQL(dropTableLoaiChiTietHoaDon);
-            db.execSQL(dropTableCTSanPham);
+
             db.execSQL(dropTableGioHang);
             onCreate(db);
         }
--- app/src/main/java/com/example/duan1/untils.java
+++ app/src/main/java/com/example/duan1/untils.java
@@ -1,6 +1,5 @@
 package com.example.duan1;
 
-import com.example.duan1.Model.DonHang;
 import com.example.duan1.Model.GioHang;
 import com.example.duan1.Model.HoaDon;
 
@@ -12,7 +11,7 @@ public class untils {
 
     public static List<GioHang> mangMuaHang = new ArrayList<>();
 
-    public static List<DonHang> donHangAdmin;
+
 
     public static List<HoaDon> hoadonAdmin;
 }
